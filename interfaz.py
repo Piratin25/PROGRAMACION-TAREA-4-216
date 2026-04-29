649563dec9cb497dfbdedcaeb3b4904767f65a6e
Paula <pauuvar0@gmail.com>
2026-04-29 11:35:34 -0500

    Interfaz grafica

--- interfaz.py
+++ interfaz.py
@@ -9,6 +9,8 @@ class App:
         
         self.crear_widgets()
         
+        self.reservas = []
+        
     def crear_widgets(self):
         #------ CLIENTE ------
         ttk.Label(self.root, text="Nombre").pack()
@@ -31,6 +33,19 @@ class App:
         #----- BOTON ------
         ttk.Button(self.root, text="Crear Reserva", command=self.crear_reserva).pack(pady=10)
         
+        ttk.Button(self.root, text="Cancelar Reserva", command=self.cancelar_reserva).pack()
+        #----- TABLA ------
+        self.tabla = ttk.Treeview(self.root, columns=("Cliente", "Servicio", "Estado", "Costo"), show="headings")
+        
+        self.tabla.heading("Cliente", text="Cliente")
+        self.tabla.heading("Servicio", text="Servicio")
+        self.tabla.heading("Estado", text="Estado")
+        self.tabla.heading("Costo", text="Costo")
+        
+        self.tabla.pack(pady=10)
+        
+        
+        
     def crear_reserva(self):
         try:
             nombre = self.nombre.get()
@@ -50,7 +65,32 @@ class App:
             reserva = Reserva(cliente, servicio)
             reserva.confirmar()
             
+            costo = servicio.calcular_costo()
+            
+            self.reservas.append(reserva)
+            
+            self.tabla.insert("", "end", values=(cliente.get_nombre(), servicio.descripcion(), reserva.estado, costo))
+            
             messagebox.showinfo("Exito", "Reserva creada exitosamente")
         
+        except Exception as e:
+            messagebox.showerror("Error", str(e))
+    
+    def cancelar_reserva(self):
+        try:
+            seleccion = self.tabla.selection()
+            
+            if not seleccion:
+                raise Exception("Seleccione una reserva")
+            
+            index = self.tabla.index(seleccion)
+            reserva = self.reservas[index]
+            
+            reserva.cancelar()
+            
+            costo = reserva.servicio.calcular_costo()
+            
+            self.tabla.item(seleccion, values=(reserva.cliente.get_nombre(), reserva.servicio.descripcion(), reserva.estado, costo))
+            
         except Exception as e:
             messagebox.showerror("Error", str(e))
